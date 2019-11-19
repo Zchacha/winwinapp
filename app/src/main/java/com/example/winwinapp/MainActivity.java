baf288e828aae342a759971264d416428186c299
chacha <60050201@kmitl.ac.th>
2019-11-19 10:00:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/winwinapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.winwinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Button bLoginLink,bRegisterLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bLoginLink = (Button) findViewById(R.id.bLoginLink);
+        bRegisterLink = (Button) findViewById(R.id.bRegisterLink);
+        bLoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+        bRegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegister();
+            }
+        });
+    }
+    public void openLogin(){
+        Intent intent = new Intent(this,login.class);
+        startActivity(intent);
+    }
+    public void openRegister(){
+        Intent intent = new Intent(this,register.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/winwinapp/register.java
@@ -0,0 +1,38 @@
+package com.example.winwinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    EditText etFirstname,etLastname,etEmail,etUsername,etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etFirstname = (EditText) findViewById(R.id.etFirstname);
+        etLastname = (EditText) findViewById(R.id.etLastname);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bRegister = (Button) findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bRegister:
+                break;
+        }
+    }
+}
