54609bcdf649a16dde41f12ceebab6a9901bda82
chacha <60050201@kmitl.ac.th>
2019-11-19 22:24:06 +0700

    forgot

--- app/src/main/java/com/example/winwinapp/forgot.java
+++ app/src/main/java/com/example/winwinapp/forgot.java
@@ -2,7 +2,11 @@ package com.example.winwinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class forgot extends AppCompatActivity {
 
