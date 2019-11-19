9d6e3aeebe95451479efab03d4d42eae22fed46c
chacha <60050201@kmitl.ac.th>
2019-11-19 13:34:38 +0700

    login to home

--- app/src/main/java/com/example/winwinapp/login.java
+++ app/src/main/java/com/example/winwinapp/login.java
@@ -2,6 +2,7 @@ package com.example.winwinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.EditText;
 
 public class login extends AppCompatActivity implements View.OnClickListener {
 
-    Button bLogin;
+    private Button bLogin;
     EditText etUsername,etPassword;
 
     @Override
@@ -29,6 +30,8 @@ public class login extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v){
         switch (v.getId()){
             case R.id.bLogin:
+                Intent intent = new Intent(this,Home.class);
+                startActivity(intent);
                 break;
         }
     }
