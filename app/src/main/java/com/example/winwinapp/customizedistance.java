2e8c8afd1b5e4faa95f1d3f60181b1691d50c27b
60050148 <60050148@kmitl.ac.th>
2019-11-20 15:01:04 +0700

    กำหนดระยะทาง

---
+++ app/src/main/java/com/example/winwinapp/customizedistance.java
@@ -0,0 +1,14 @@
+package com.example.winwinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customizedistance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customizedistance);
+    }
+}
