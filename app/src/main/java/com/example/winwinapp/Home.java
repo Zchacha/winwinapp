a79658a426f20d53ca4fd25bb7a31fbcce72d296
pornpailin charoenthong <60050211@kmitl.ac.th>
2019-11-20 14:12:02 +0700

    editprofile

--- app/src/main/java/com/example/winwinapp/Home.java
+++ app/src/main/java/com/example/winwinapp/Home.java
@@ -1,5 +1,7 @@
 package com.example.winwinapp;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,11 +22,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
+    private TextView etprofile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +55,17 @@ public class Home extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        etprofile = (TextView) findViewById(R.id.etprofile);
+        etprofile.setPaintFlags(etprofile.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        etprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView etprofile = (TextView) findViewById(R.id.etprofile);
+                Intent intent = new Intent(Home.this, editprofile.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -65,5 +80,11 @@ public class Home extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void openEditprofile(){
+
+        Intent intent = new Intent(this,editprofile.class);
+        startActivity(intent);
     }
 }
