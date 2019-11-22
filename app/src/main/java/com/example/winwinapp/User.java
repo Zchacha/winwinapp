48574d38a75a519b061a7eb7197f84a7de724f79
chacha <60050201@kmitl.ac.th>
2019-11-22 15:24:11 +0700

    login

--- app/src/main/java/com/example/winwinapp/User.java
+++ app/src/main/java/com/example/winwinapp/User.java
@@ -1,15 +1,16 @@
 package com.example.winwinapp;
 
 public class User {
-    public String fistname,lastname,username,email;
+    public String fistname,lastname,username,email,password;
 
-    public User(String firstname, String lastname, String username, String email, String password){
+    public User(String firstname, String lastname, String username, String email){
     }
 
-    public User(String fistname, String lastname, String username, String email) {
+    public User(String fistname, String lastname, String username, String email,String password) {
         this.fistname = fistname;
         this.lastname = lastname;
         this.username = username;
         this.email = email;
+        this.password = password;
     }
 }
