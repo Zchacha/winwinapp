ee7c2fbb8de5b3a0722a3f35f1d78e6aea341467
60050148 <60050148@kmitl.ac.th>
2019-11-24 15:29:59 +0700

    ทำเชื่อมไป dial call

--- app/src/main/java/com/example/winwinapp/complaint.java
+++ app/src/main/java/com/example/winwinapp/complaint.java
@@ -2,13 +2,31 @@ package com.example.winwinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class complaint extends AppCompatActivity {
 
+    private Button tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
+
+        tel = findViewById(R.id.tel1548);
+        tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = "1548";
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse(phoneNumber));
+             // error permision
+                //  startActivity(intent);
+
+            }
+        });
     }
 }
