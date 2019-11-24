385587ab80ea5fce4c1881018b2982fc8900aba1
chacha <60050201@kmitl.ac.th>
2019-11-24 14:05:02 +0700

    5555

--- app/src/main/java/com/example/winwinapp/forgot.java
+++ app/src/main/java/com/example/winwinapp/forgot.java
@@ -8,6 +8,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,6 +22,7 @@ public class forgot extends AppCompatActivity {
 
     private EditText etEmail;
     private Button bForgot;
+    private ProgressBar loadingProgress;
     private FirebaseAuth auth;
 
     @SuppressLint("WrongViewCast")
@@ -29,12 +31,16 @@ public class forgot extends AppCompatActivity {
         setContentView(R.layout.activity_forgot);
         etEmail = (EditText) findViewById(R.id.etEmail);
         bForgot = (Button) findViewById(R.id.bForgot);
+        loadingProgress = findViewById(R.id.progressBar1);
         auth = FirebaseAuth.getInstance();
+        loadingProgress.setVisibility(View.INVISIBLE);
 
         bForgot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = etEmail.getText().toString().trim();
+                bForgot.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplication(), "กรุณากรอกอีเมล", Toast.LENGTH_SHORT).show();
                     return;
@@ -45,11 +51,13 @@ public class forgot extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(forgot.this, "ได้ทำการส่งรหัสผ่านไปทางอีเมลแล้ว", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(forgot.this, "กรุณาตรวจสอบอีเมล", Toast.LENGTH_SHORT).show();
 
                                 } else {
                                     Toast.makeText(forgot.this, "เกิดข้อผิดพลาด", Toast.LENGTH_SHORT).show();
                                 }
+                                bForgot.setVisibility(View.VISIBLE);
+                                loadingProgress.setVisibility((View.INVISIBLE));
                             }
                         });
             }
